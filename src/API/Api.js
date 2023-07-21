3464ed0b884a6bfe5eec4b5264fb61ea08cb15a8
Itssyhdnn <121994348+Syenvi@users.noreply.github.com>
2023-07-21 15:01:31 +0700

    Update Api.js

--- src/API/Api.js
+++ src/API/Api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const instance = axios.create({
-    baseURL : 'https://ccba-180-254-89-214.ngrok-free.app/api',
+    baseURL : 'https://lurasyah.muhammadiyahexpo.com/api',
 })
 
 export default instance
